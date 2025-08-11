9eb91b5799b0e87204a3363f6c7db983600443db
grassblock <i@gb0.dev>
2025-08-11 19:06:03 +0800

    feat: add passable arguments to entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-bash $SCRIPT_DIR/init.sh
-bash $SCRIPT_DIR/merge.sh
+args=("$@")
+if [ ${args[0]} == '--copy-src-when-init' ]; then
+	bash $SCRIPT_DIR/init.sh --also-copy-src
+else
+	bash $SCRIPT_DIR/init.sh
+fi
+if [ ${args[1]} == '--copy-config-when-merge' ]; then
+	bash $SCRIPT_DIR/merge.sh
+else
+	bash $SCRIPT_DIR/merge.sh
+fi
 bash $SCRIPT_DIR/build.sh
