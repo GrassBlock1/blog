a2d154a419eda749f2b7b829a121269c803f9f28
grassblock <i@gb0.dev>
2025-08-13 17:59:43 +0800

    fix: directory is not cleaned up when deployment failed

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo -e "[build] starting build\n"
 cd mercury || { echo -e "[build] [error] mercury directory not found, exiting\n" ; exit 1; }
-pnpm build || { echo -e "[build] [error] build failed\n" ; echo -e "[build] cleaning up\n" ; rm -rf ./mercury ; exit 1; }
+pnpm build || { echo -e "[build] [error] build failed\n" ; echo -e "[build] cleaning up\n" ; cd .. && rm -rf ./mercury ; exit 1; }
 cd ..
 # move the output to make the deployment service use with Astro correctly
 mv ./mercury/dist ./
